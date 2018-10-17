08c61ddb9fdd862d09eb9783d134d81f3d2de71e
Niklas Bolander <niklas.bolander@gmail.com>
2018-10-17 22:15:54 +0200

    Aliases for OSX

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,2 +1,10 @@
-alias ls="ls --color=auto"
-alias grep="grep --color=auto"
+case "$(uname -s)" in
+    Darwin)
+        alias ls="ls -G"
+        alias grep="grep --color=auto"
+        ;;
+    *)
+        alias ls="ls --color=auto"
+        alias grep="grep --color=auto"
+        ;;
+esac
