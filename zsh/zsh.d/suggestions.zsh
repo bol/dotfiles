2b6b8eaee28e022a9ccd98f987eaf76fa70475c3
Niklas Bolander <niklas.bolander@gmail.com>
2019-06-28 17:24:59 +0200

    Add autosuggestion plugin

---
+++ zsh/zsh.d/suggestions.zsh
@@ -0,0 +1,4 @@
+zplugin ice wait"0" atload"_zsh_autosuggest_start"
+zplugin light zsh-users/zsh-autosuggestions
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#909090,underline"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
