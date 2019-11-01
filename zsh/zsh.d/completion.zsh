8b55b307ea7ccfaead9079b90d55696c00d09c7b
Niklas Bolander <niklas.bolander@gmail.com>
2019-11-01 14:40:33 +0100

    Dump

--- zsh/zsh.d/completion.zsh
+++ zsh/zsh.d/completion.zsh
@@ -1,7 +1,6 @@
 zplugin ice wait"0" blockf
 zplugin light zsh-users/zsh-completions
 zstyle ':completion:*' menu select
-setopt COMPLETE_ALIASES
 
 zplugin ice wait"0" blockf
 zplugin light "greymd/docker-zsh-completion"
