c58c105de2a9ff2aa796a6734e8fa003cc6e7b12
Niklas Bolander <niklas.bolander@gmail.com>
2019-11-26 13:12:13 +0100

    Locale fixes

--- zsh/zsh.d/locale.zsh
+++ zsh/zsh.d/locale.zsh
@@ -8,12 +8,12 @@ set_if_unset() {
 }
 
 # Set a sensible default of english language and Swedish units if locale is not set.
-set_if_unset 'LANG', 'en_US.UTF-8'
-set_if_unset 'LC_MEASUREMENT', 'sv_SE.UTF-8'
-set_if_unset 'LC_MONETARY', 'sv_SE.UTF-8'
-set_if_unset 'LC_NUMERIC', 'sv_SE.UTF-8'
-set_if_unset 'LC_PAPER', 'sv_SE.UTF-8'
-set_if_unset 'LC_TIME', 'sv_SE.UTF-8'
+set_if_unset 'LANG' 'en_US.UTF-8'
+set_if_unset 'LC_MEASUREMENT' 'sv_SE.UTF-8'
+set_if_unset 'LC_MONETARY' 'sv_SE.UTF-8'
+set_if_unset 'LC_NUMERIC' 'sv_SE.UTF-8'
+set_if_unset 'LC_PAPER' 'sv_SE.UTF-8'
+set_if_unset 'LC_TIME' 'sv_SE.UTF-8'
 
 # LC_ALL is meant for debugging and should never be set by default.
 if [[ -z "${LC_ALL}" ]]; then
