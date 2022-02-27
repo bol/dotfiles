40dd9365d43793ac8b8d403b8fc92d2467215a97
Niklas Bolander <niklas@bolander.dev>
2022-02-27 22:39:41 +0100

    Switched nvm to nodenv

--- zsh/zsh.d/env_nodejs.zsh
+++ zsh/zsh.d/env_nodejs.zsh
@@ -1,13 +1,11 @@
-function activate_nvm() {
-    [[ -e $HOME/.nvm ]] || {
-        read -q '?$HOME/.nvm does not exist, do you want to install it? ' || return -1
-            print '\ninstalling nvm'
-            git clone https://github.com/creationix/nvm.git ~/.nvm
-
-            # Checkout the latest version tag
-            cd ~/.nvm
-            git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
+function activate_nodenv() {
+    [[ -e $HOME/.nodenv ]] || {
+        read -q '?$HOME/.nodenv does not exist, do you want to install it? ' || return -1
+            print '\ninstalling nodenv'
+            git clone https://github.com/nodenv/nodenv.git $HOME/.nodenv
+            mkdir -p $HOME/.nodenv/plugins
+            git clone https://github.com/nodenv/node-build.git $HOME/.nodenv/plugins/node-build
         }
-    export NVM_DIR="$HOME/.nvm"
-    . "$NVM_DIR/nvm.sh"
+    export PATH="$HOME/.nodenv/bin:$PATH"
+    eval "$(nodenv init -)"
 }
