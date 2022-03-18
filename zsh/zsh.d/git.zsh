aa3ff8c68cedcfd386000e522b51671f25e63cc4
Niklas Bolander <niklas.bolander@bambora.com>
2022-03-18 21:08:22 +0100

    Remove diff-so-fancy
    Replaced by delta

--- zsh/zsh.d/git.zsh
+++ zsh/zsh.d/git.zsh
@@ -2,4 +2,3 @@ zplugin ice lucid as"program" pick"$ZPFX/bin/git-alias" make"PREFIX=$ZPFX" nocom
 zplugin light tj/git-extras
 
 zplugin ice lucid as"program" pick"bin/git-dsf"
-zplugin light zdharma/zsh-diff-so-fancy
