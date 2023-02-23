bbbabc17098790e52754375b72d09086ff5db73b
Niklas Bolander <69475+bol@users.noreply.github.com>
2023-02-23 10:40:29 +0100

    nvim - Reduce auto-installed syntaxes for treesitter
    Many of the maintained syntaxes are of poor quality and comes with external dependencies.
    Maintain a whitelist of well working ones so that we don't wake up to a broken nvim one morning.

--- nvim/nvim_config/lua/plugins/treesitter.lua
+++ nvim/nvim_config/lua/plugins/treesitter.lua
@@ -5,8 +5,29 @@ use {
     run = ':TSUpdate'
 }
 
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained",
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "diff",
+        "dockerfile",
+        "gitattributes",
+        "go",
+        "gomod",
+        "hcl",
+        "html",
+        "json",
+        "make",
+        "markdown",
+        "rst",
+        "terraform",
+        "toml",
+        "vim",
+        "yaml",
+    },
     sync_install = false,
     highlight = {
         enable = true,
