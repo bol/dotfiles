ebad0e1446b7e9cec3c87c8ae09265ab3cd6ac78
Niklas Bolander <niklas@bolander.dev>
2023-02-28 23:13:52 +0100

    Move markup support to separate module

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,18 +1,4 @@
-# Query JSON
-# Use gojq instead of regular jq as the original has broken base64 handling and is no longer maintained.
-# This also gives us YAML support
-# https://github.com/stedolan/jq/issues/1931
-alias jq='gojq'
 
-# Colorize JSON
-alias jc="bat --language JSON --theme 'Solarized (dark)' --style=plain"
-
-# Query YAML
-alias yq='gojq --yaml-output --yaml-input'
-# Convert JSON to YAML
-alias j2y='gojq --yaml-output'
-# Colorize YAML
-alias yc="bat --language YAML --theme 'Solarized (dark)' --style=plain"
 
 case "$(uname -s)" in
     Darwin)
