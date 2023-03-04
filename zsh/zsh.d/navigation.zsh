769f221f6a8ef330e29da1da6802ff7f732fc9f5
Niklas Bolander <niklas@bolander.dev>
2023-03-04 22:12:20 +0100

    Replace zsh-z with zoxide

--- zsh/zsh.d/navigation.zsh
+++ zsh/zsh.d/navigation.zsh
@@ -1,2 +1,13 @@
-zinit ice wait lucid blockf
-zinit light agkozak/zsh-z
+if ! (( $+commands[zoxide] )); then
+  print "The zoxide command was not found on your path.\n"
+  case "$(uname -s)" in
+      Darwin)
+        read -q "?Do you want to install it with homebrew? " || return -1
+        print ''
+        brew install "zoxide"
+        ;;
+      *)
+        ;;
+  esac
+fi
+eval "$(zoxide init zsh)"
