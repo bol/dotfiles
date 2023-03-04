0bb7dfad27c94e40a41c6761e9bdfbb9849a8339
Niklas Bolander <niklas@bolander.dev>
2023-03-04 22:21:00 +0100

    Wezterm support

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+local config = {
+    default_prog = { '/opt/homebrew/bin/zsh' },
+
+    color_scheme = "Builtin Solarized Dark",
+
+    font = wezterm.font("MesloLGM Nerd Font", {weight="Regular", stretch="Normal", style="Normal"}),
+    font_size = 16.0,
+    hide_tab_bar_if_only_one_tab = true,
+    audible_bell = "Disabled",
+}
+
+return config
