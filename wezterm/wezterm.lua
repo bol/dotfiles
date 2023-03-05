d4b550659bc7bda38c2d705de3786f4280f98215
Niklas Bolander <niklas@bolander.dev>
2023-03-05 22:43:39 +0100

    Wezterm: MacOS tmux support
    Needs full path as homebrew is initialized in shell startup after the terminal has launched.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,18 @@
 local wezterm = require("wezterm")
 
 local config = {
-    default_prog = { '/usr/bin/env', 'tmux' },
-
     color_scheme = "Builtin Solarized Dark",
 
-    font = wezterm.font("MesloLGM Nerd Font", {weight="Regular", stretch="Normal", style="Normal"}),
+    font = wezterm.font("MesloLGM Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }),
     font_size = 16.0,
     hide_tab_bar_if_only_one_tab = true,
     audible_bell = "Disabled",
 }
+
+if wezterm.target_triple == 'aarch64-apple-darwin' or wezterm.target_triple == 'x86_64-apple-darwin' then
+    config.default_prog = { '/opt/homebrew/bin/tmux' }
+else
+    config.default_prog = { '/usr/bin/env', 'zsh' }
+end
 
 return config
