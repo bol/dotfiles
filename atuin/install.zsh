bd074fa4e23b4c8da631a96f03c13795b567cce2
Niklas Bolander <niklas@bolander.dev>
2024-02-22 22:16:53 +0100

    Install script formatting

--- atuin/install.zsh
+++ atuin/install.zsh
@@ -4,7 +4,7 @@ if ! (($+commands[atuin])); then
     print "The atuin command was not found on your path.\n"
     case "$(uname -s)" in
         Darwin)
-          read -q "?Do you want to install it from homebrew? " || return -1
+          read -q "?Do you want to install it from homebrew? " || return 1
           print ''
           brew install atuin
             ;;
