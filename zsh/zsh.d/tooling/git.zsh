9d5c751e0543fe2263f4145cb121d0addf854481
Niklas Bolander <niklas@bolander.dev>
2024-03-05 20:49:02 +0100

    Zsh - Improve package detection
    In the case of git-delta the package name and name of the binary used to
    detect if it's installed are different.

--- zsh/zsh.d/tooling/git.zsh
+++ zsh/zsh.d/tooling/git.zsh
@@ -1,4 +1,4 @@
 zinit ice as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX"
 zinit light tj/git-extras
 
-__ensure_package_is_installed git-delta
+__ensure_package_is_installed delta git-delta
