7d03519784d933ca2137daf5a989f5cdcb1f5495
Niklas Bolander <niklas@bolander.dev>
2024-03-14 20:29:44 +0100

    Remove Atuin
    Too disruptive.

--- zsh/zsh.d/tooling/history.zsh
+++ zsh/zsh.d/tooling/history.zsh
@@ -10,15 +10,3 @@ setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
-
-# Use Atuin if installed, otherwise fallback to zsh native history
-if [[ $commands[atuin] ]]; then
-  ATUIN_NOBIND=1 zinit load atuinsh/atuin
-  bindkey -M emacs '^r' atuin-search
-  bindkey -M viins '^r' atuin-search-viins
-  bindkey -M vicmd '/' atuin-search
-  bindkey -M vicmd 'k' atuin-up-search-vicmd
-else
-  zinit ice wait lucid blockf
-  zinit light zdharma-continuum/history-search-multi-word
-fi
