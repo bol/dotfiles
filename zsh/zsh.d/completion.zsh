9c11531b2416d4ae7196def2a7a989878faef02c
Niklas Bolander <niklas@bolander.dev>
2024-03-27 22:08:24 +0100

    Zsh - Remove Docker and Bazel completions
    They are unused.

--- zsh/zsh.d/completion.zsh
+++ zsh/zsh.d/completion.zsh
@@ -2,10 +2,4 @@ zinit ice lucid wait blockf
 zinit light "zsh-users/zsh-completions"
 zstyle ':completion:*' menu select
 
-zinit ice lucid wait blockf
-zinit light "greymd/docker-zsh-completion"
-
-zinit ice lucid wait blockf
-zinit snippet "https://raw.githubusercontent.com/bazelbuild/bazel/master/scripts/zsh_completion/_bazel"
-
 autoload -Uz compinit && compinit
