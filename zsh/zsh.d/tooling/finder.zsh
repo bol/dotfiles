97e68b8067dc0e1eafcf56014d4be842c9d00ce6
Niklas Bolander <niklas@bolander.dev>
2024-04-05 23:56:35 +0200

    Zsh - Primitive visual file manager

--- zsh/zsh.d/tooling/finder.zsh
+++ zsh/zsh.d/tooling/finder.zsh
@@ -1,2 +1,10 @@
 __ensure_package_is_installed fzf
 eval "$(fzf --zsh)"
+
+# Primitive visual file manager
+function fm() {
+    fzf \
+      --preview 'bat --line-range=:200 {}' --preview-window '~3' \
+      --multi --bind 'enter:become(nvim {+})' \
+      --bind 'ctrl-h:become(hexdump -C {})'
+}
