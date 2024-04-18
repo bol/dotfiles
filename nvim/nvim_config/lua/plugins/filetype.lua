008af29178a8019cc0514bb43b07b2b7b1b74c02
Niklas Bolander <niklas.bolander.external@worldline.com>
2024-04-18 15:38:36 +0200

    nvim: remove filetype plugin
    Not needed anymore

--- nvim/nvim_config/lua/plugins/filetype.lua
+++
@@ -1,8 +0,0 @@
-local use = require("packer").use
-
-use {
-  'nathom/filetype.nvim',
-  config = function()
-    require('filetype').setup({})
-  end
-}
--- nvim/nvim_config/lua/plugins/init.lua
+++ nvim/nvim_config/lua/plugins/init.lua
@@ -7,7 +7,6 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   local plugins = {
-    'plugins.filetype',
     'plugins.theme',
     'plugins.treesitter',
   }
