2830fde8cddc3d4631e3e2ec0c931237dcb7083a
Niklas Bolander <niklas@bolander.dev>
2025-08-25 11:12:49 +0200

    NVim - More specific links
    Packer compiles files in the config directory. Only link in our specific files so that they're not covered by version control.

--- nvim/install.zsh
+++ nvim/install.zsh
@@ -1,6 +1,7 @@
 0=${${(M)${0::=${(%):-%x}}:#/*}:-$PWD/$0}
 
-link_conf $1/nvim_config ~/.config/nvim
+link_conf $1/nvim_config/init.lua ~/.config/nvim/init.lua
+link_conf $1/nvim_config/lua ~/.config/nvim/lua
 git_clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo -e "\tSyncing neovim plugins"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 2> /dev/null
