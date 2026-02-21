0698f66893dadfec9dfce92e6e9bc987c4ca187a
Niklas Bolander <niklas@bolander.dev>
2026-02-21 21:40:59 +0100

    feat(zsh): improve history dedup and search behavior

--- zsh/zsh.d/tooling/history.zsh
+++ zsh/zsh.d/tooling/history.zsh
@@ -6,7 +6,10 @@ SAVEHIST=10000
 setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_find_no_dups      # skip duplicate entries when searching history
 setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_reduce_blanks     # trim superfluous blanks before recording entries
+setopt hist_save_no_dups      # remove older duplicate entries when writing history
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
