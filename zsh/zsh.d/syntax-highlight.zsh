f711950ea0a10531f38ca5c1fb6b1c538699f96e
Niklas Bolander <niklas@bolander.dev>
2026-02-21 23:13:48 +0100

    fix(zsh): improve interactive comment readability

--- zsh/zsh.d/syntax-highlight.zsh
+++ zsh/zsh.d/syntax-highlight.zsh
@@ -1,2 +1,7 @@
+typeset -gA FAST_HIGHLIGHT_STYLES
+# Keep interactive comments readable on dark terminals.
+FAST_HIGHLIGHT_STYLES[comment]='fg=250'
+FAST_HIGHLIGHT_STYLES[freecomment]='fg=250'
+
 zinit ice wait lucid
 zinit light zdharma-continuum/fast-syntax-highlighting
